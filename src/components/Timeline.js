7a9c7f54dd95abed469bee49fe04628b02076a09
Jim Lu <jyunyan.lu@gmail.com>
2022-01-25 02:22:42 -0500

    custom hook use-photos

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import usePhotos from '../hooks/use-photos';
 
 const Timeline = () => {
+  // we need to get the logged in user's photos (hook)
+  const { photos } = usePhotos();
+  // on loading the photos, we need to use react skeleton
+  // if we have photos, render them (create a post component)
+  // if the user has no photos, tell them to create some photos
+
   return (
     <div className="container col-span-2">
-      <p>this is the timeline</p>
+      {!photos ? (
+        <>
+          <Skeleton count={4} width={640} height={500} className="mb-5" />
+        </>
+      ) : photos?.length > 0 ? (
+        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+      ) : (
+        <p className="text-center text-2xl">Follow people to see photos!</p>
+      )}
     </div>
   );
 };
